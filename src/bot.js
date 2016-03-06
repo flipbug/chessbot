a914a6da2704feae2020a16b8c137dda5ba142fe
Daniel Milenkovic <daniel.milenkovic@flipbug.ch>
2016-03-06 22:07:22 +0100

    move bot out of the core

--- src/bot.js
+++ src/bot.js
@@ -3,40 +3,40 @@
  *
  * @author Daniel Milenkovic
  */
-function Bot(side, board) {
+game.Bot = function(side, board) {
 	this.side = side;
 	this.board = board;
 
-	this.simulator = new Simulator();
+	this.simulator = new game.core.Simulator();
 	this.enabled = 1;
 }
 
-Bot.prototype.makeMove = function() {
+game.Bot.prototype.makeMove = function() {
 	var gameTree = this.simulator.generateGameTree(this.side, this.board, 0);
 	var move = this.findBestMove(gameTree);
 
 	var event = new CustomEvent(EVENT.PIECE_MOVED, {
 		'detail': {
-			'newPos': move.newPos,
-			'oldPos': move.oldPos
+			'target': move.newPos,
+			'start': move.oldPos
 		}
 	});
 	window.dispatchEvent(event);
 };
 
-Bot.prototype.findBestMove = function(gameTree) {
+game.Bot.prototype.findBestMove = function(gameTree) {
 	//todo: alphabeta implementation
 	// console.log(gameTree);
 	var move = gameTree[Math.floor(Math.random() * gameTree.length)];
 	var actualMove = move.moves[Math.floor(Math.random() * move.moves.length)];
 	// console.log(actualMove);
 	return {
-		'newPos': new Vector(actualMove.x, actualMove.y),
-		'oldPos': move.piece.position
+		'target': new Vector(actualMove.x, actualMove.y),
+		'start': move.piece.position
 	}
 };
 
-Bot.prototype.findBestMoveByValue = function(moves) {
+game.Bot.prototype.findBestMoveByValue = function(moves) {
 	var m = -Infinity;
 
 	for (var i = 0; i < moves.length; ++i) {
